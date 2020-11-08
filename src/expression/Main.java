e2977364ec3cd9f1b3da882f7b0fa1be9a455c9f
TanyaCheremkhina <tanyacheremkhina@gmail.com>
2020-11-08 21:43:57 +0300

    Main.java modified

--- src/expression/Main.java
+++ src/expression/Main.java
@@ -1,5 +1,6 @@
 package expression;
 
+import expression.generic.DoubleNumber;
 import expression.generic.GenericTabulator;
 import expression.generic.IntegerNumber;
 import expression.generic.Tabulator;
@@ -11,26 +12,8 @@ public class Main {
         ExpressionParser<Integer> parser = new ExpressionParser<>(new IntegerNumber<>());
         System.out.println(parser.parse("x + y * z").evaluate(2, 2, 2)); // 6
 
-        //Example of using tabulator:
-        Tabulator tabulator = new GenericTabulator();
-        int x1 = 1, y1 = 1, z1 = 1, x2 = 2, y2 = 2, z2 = 2;
-        Object[][][] arr = tabulator.tabulate("l", "x + y * z", x1, x2, y1, y2, z1, z2);
-        for (int x = x1; x <= x2; x++) {
-            for (int y = y1; y <= y2; y++) {
-                System.out.print("[");
-                for (int z = z1; z <= z2; z++) {
-                    System.out.print(arr[x - x1][y - y1][z - z1] + " ");
-                }
-                System.out.println("]");
-            }
-            System.out.println();
-        }
-        // Result:
-
-        // [2 3 ]
-        // [3 5 ]
-
-        // [3 4 ]
-        // [4 6 ]
+        // Example of using for double:
+        ExpressionParser<Double> doubleParser = new ExpressionParser<>(new DoubleNumber<>());
+        System.out.println(doubleParser.parse("x + y * z").evaluate(2.2, 2.2, 2.2)); // 7.040000000000001
     }
 }
